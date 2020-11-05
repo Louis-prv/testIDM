feed4264a7c53566760ec5bf2e79c6dd5f613891
Louis PREVOST <louis77.prevost@hotmail.fr>
2020-11-05 15:02:49 +0100

    First Commit

---
+++ testIDM/src/main/java/testIDM/RaceResults.java
@@ -0,0 +1,10 @@
+package testIDM;
+
+public class RaceResults {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
---
+++ testIDM/src/main/java/testIDM/TimeDuration.java
@@ -0,0 +1,10 @@
+package testIDM;
+
+public class TimeDuration {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
